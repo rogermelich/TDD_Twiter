d57d549b082ee91e741787c9fe9364ee9b3d9274
rogermelich <rogermelich@gmail.com>
2016-02-15 19:54:45 +0100

    Resolve problemes

---
+++ tests/TestViewAnotherUserTweets.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\User;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class TestViewAnotherUserTweets extends TestCase
+{
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testViewAnotherTweets()
+    {
+        $user = factory(User::class->create(['username' => 'johndoe']));
+        $tweet = factory(Tweet::class->create(['body' => 'My first tweet']));
+        $user-tweets()->save($tweet);
+
+        $this->visit('/johndoe')
+             ->see('My firts Tweet');
+    }
+}
